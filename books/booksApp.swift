6c3f4df91c1f1fc14c8af659e448712ac47aa8f9
Tyler Reed <tylerreedytlearning@gmail.com>
2022-04-01 10:17:54 -0400

    Initial Commit

---
+++ books/booksApp.swift
@@ -0,0 +1,17 @@
+//
+//  booksApp.swift
+//  books
+//
+//  Created by Tyler Reed on 4/1/22.
+//
+
+import SwiftUI
+
+@main
+struct booksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
